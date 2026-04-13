1a88008ed33e1c84386260cd4e6bfb6cf1a02f33
Ichinoseyang <a156226@163.com>
2026-04-13 21:14:14 +0800

    init_exp_10

---
+++ exp_10/MEM_WB_reg.v
@@ -0,0 +1,52 @@
+module MEM_WB_reg (
+    input  wire        clk,
+    input  wire        rst,
+
+    input  wire        mem_ready_go,
+    input  wire        wb_allowin,
+
+    input  wire        mem_valid,
+    input  wire [31:0] mem_pc,
+    input  wire        mem_rf_we,
+    input  wire [ 4:0] mem_rf_waddr,
+    input  wire [31:0] mem_rf_wdata,
+
+    output reg         wb_valid,
+    output reg  [31:0] wb_pc,
+    output reg         wb_rf_we,
+    output reg  [ 4:0] wb_rf_waddr,
+    output reg  [31:0] wb_rf_wdata
+);
+
+    always @(posedge clk) begin
+        if (rst) begin
+            wb_valid    <= 1'b0;
+            wb_pc       <= 32'b0;
+            wb_rf_we    <=  1'b0;
+            wb_rf_waddr <=  5'b0;
+            wb_rf_wdata <= 32'b0;
+        end
+        else if (!wb_allowin) begin
+            wb_valid    <= wb_valid;
+            wb_pc       <= wb_pc;
+            wb_rf_we    <= wb_rf_we;
+            wb_rf_waddr <= wb_rf_waddr;
+            wb_rf_wdata <= wb_rf_wdata;
+        end
+        else if (!mem_ready_go) begin
+            wb_valid    <= 1'b0;
+            wb_pc       <= 32'b0;
+            wb_rf_we    <=  1'b0;
+            wb_rf_waddr <=  5'b0;
+            wb_rf_wdata <= 32'b0;
+        end
+        else begin
+            wb_valid    <= mem_valid;
+            wb_pc       <= mem_pc;
+            wb_rf_we    <= mem_rf_we;
+            wb_rf_waddr <= mem_rf_waddr;
+            wb_rf_wdata <= mem_rf_wdata;
+        end
+    end
+
+endmodule
